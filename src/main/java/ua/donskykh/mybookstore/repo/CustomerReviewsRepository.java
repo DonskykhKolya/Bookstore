6a19c5d91a273d73c7a6251233e006c5041dd9f1
Nikolaj Donskikh <ndonskih63@gmail.com>
2019-05-01 01:06:15 +0200

    Revert "Initial commit"
    This reverts commit b7f1fd8e536edb4a12dd958d6d3243c073df90e8.

--- src/main/java/ua/donskykh/mybookstore/repo/CustomerReviewsRepository.java
+++
@@ -1,9 +0,0 @@
-package ua.donskykh.mybookstore.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ua.donskykh.mybookstore.domain.CustomerReviews;
-
-@Repository("customerReviewRepository")
-public interface CustomerReviewsRepository extends JpaRepository<CustomerReviews, Long> {
-}
